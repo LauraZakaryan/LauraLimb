c8eccd2a80454c4c61bf8ca3ea899ef40b7f2c5c
armansargsyan <=>
2023-10-11 18:55:58 +0400

    Changed: Create class UserManagment.

--- Task1.php
+++ Task1.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 
 class UserManagement
 {
@@ -7,6 +6,7 @@ class UserManagement
 
     public function __construct()
     {
+        session_start();
         if (!isset($_SESSION['user_data'])) {
             $_SESSION['user_data'] = [];
         }
@@ -37,17 +37,18 @@ class UserManagement
 
     public function drawForm()
     {
-        echo '
-               <h1>Registration</h1> 
-    
-               <form action="" method="post" enctype="multipart/form-data">
-               First name: <input type="text" name="fname"><br><br>
-               Last name: <input type="text" name="lname"><br><br>
-               Date of birth: <input type="date" id="birthdate" name="birthdate"><br><br>
-               Age: <input type="number" id="age" name="age" min="1" max="1000"><br><br>
-               About me: <textarea name="aboutme" rows="5" cols="40"></textarea><br><br>
-               Image: <input type="file" name="image"><br><br>
-               <input type="submit" name="submit" value="submit"></form>';
+        ?>
+        <h1>Registration</h1>
+
+        <form action="" method="post" enctype="multipart/form-data">
+            First name: <input type="text" name="fname"><br><br>
+            Last name: <input type="text" name="lname"><br><br>
+            Date of birth: <input type="date" id="birthdate" name="birthdate"><br><br>
+            Age: <input type="number" id="age" name="age" min="1" max="1000"><br><br>
+            About me: <textarea name="aboutme" rows="5" cols="40"></textarea><br><br>
+            Image: <input type="file" name="image"><br><br>
+            <input type="submit" name="submit" value="submit"></form>
+        <?php
     }
 
     public function getUserData()
@@ -64,40 +65,43 @@ class UserManagement
 
         $startIndex = ($currentPage - 1) * $itemsPerPage;
         $endIndex = min($startIndex + $itemsPerPage, $totalEntries);
+        ?>
 
-        echo '<style>
-             table, th, td {
-             border: 1px solid black;
-             }
-          </style>
-          <h2>Data</h2>
-          <table style="width:100%">
-          <tr>
-          <th>User</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Date of Birth</th>
-          <th>Age</th>
-          <th>About Me</th>
-          <th>Image</th>
-          <th>Edit</th>
-          </tr>';
+        <style>
+            table, th, td {
+                border: 1px solid black;
+            }
+        </style>
+        <h2>Data</h2>
+        <table style="width:100%">
+            <tr>
+                <th>User</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Date of Birth</th>
+                <th>Age</th>
+                <th>About Me</th>
+                <th>Image</th>
+                <th>Edit</th>
+            </tr>
 
-        for ($i = $startIndex; $i < $endIndex; $i++) {
-            $entry = $_SESSION['user_data'][$i];
-            echo '<tr>';
-            echo '<td>' . ($i + 1) . '</td>';
-            echo '<td>' . $entry['fname'] . '</td>';
-            echo '<td>' . $entry['lname'] . '</td>';
-            echo '<td>' . $entry['birthdate'] . '</td>';
-            echo '<td>' . $entry['age'] . '</td>';
-            echo '<td>' . $entry['aboutme'] . '</td>';
-            echo '<td><img src="' . $entry['image'] . '" width="100" height="100"></td>'; //Display the image
-            echo '<td><a href="edit_user.php?id=' . $i . '">Edit</a></td>';
-            echo '</tr>';
-        }
+            <?php
+            for ($i = $startIndex; $i < $endIndex; $i++) {
+                $entry = $_SESSION['user_data'][$i];
+                echo '<tr>';
+                echo '<td>' . ($i + 1) . '</td>';
+                echo '<td>' . $entry['fname'] . '</td>';
+                echo '<td>' . $entry['lname'] . '</td>';
+                echo '<td>' . $entry['birthdate'] . '</td>';
+                echo '<td>' . $entry['age'] . '</td>';
+                echo '<td>' . $entry['aboutme'] . '</td>';
+                echo '<td><img src="' . $entry['image'] . '" width="100" height="100"></td>'; //Display the image
+                echo '<td><a href="edit_user.php?id=' . $i . '">Edit</a></td>';
+                echo '</tr>';
+            } ?>
 
-        echo '</table>';
+        </table>
+        <?php
     }
 
     public function drawPaginationLinks()
@@ -109,9 +113,13 @@ class UserManagement
 
         for ($page = 1; $page <= $totalPages; $page++) {
             if ($page == $currentPage) {
-                echo "<span>$page</span> ";
+                ?>
+                <span>$page</span>
+                <?php
             } else {
-                echo "<a href=\"?page=$page\">$page</a> ";
+                ?>
+                <a href=\"?page=$page\">$page</a>
+                <?php
             }
         }
 
--- edit_user.php
+++ edit_user.php
@@ -18,18 +18,21 @@ class UserEditor
         if (isset($this->userData[$userId])) {
             $user = $this->userData[$userId];
 
-            echo '
-               <h1>Registration</h1>
-    
-               <form action="" method="post" enctype="multipart/form-data">
-               First name: <input type="text" name="fname" value="' . $user['fname'] . '"><br><br>
-               Last name: <input type="text" name="lname" value="' . $user['lname'] . '"><br><br>
-               Date of birth: <input type="date" id="birthdate" name="birthdate" value="' . $user['birthdate'] . '"><br><br>
-               Age: <input type="number" id="age" name="age" min="1" max="1000" value="' . $user['age'] . '"><br><br>
-               About me: <textarea name="aboutme" rows="5" cols="40">' . $user['aboutme'] . '</textarea><br><br>
-               Image: <input type="file" name="image"><br><br>
-               <img src="' . $user['image'] . '" width="100" height="100" alt="User Image"><br><br> 
-               <input type="submit" name="submit" value="Save Changes"></form>';
+            ?>
+            <h1>Registration</h1>
+
+            <form action="" method="post" enctype="multipart/form-data">
+                First name: <input type="text" name="fname" value="' . $user['fname'] . '"><br><br>
+                Last name: <input type="text" name="lname" value="' . $user['lname'] . '"><br><br>
+                Date of birth: <input type="date" id="birthdate" name="birthdate"
+                                      value="' . $user['birthdate'] . '"><br><br>
+                Age: <input type="number" id="age" name="age" min="1" max="1000" value="' . $user['age'] . '"><br><br>
+                About me: <textarea name="aboutme" rows="5" cols="40">' . $user['aboutme'] . '</textarea><br><br>
+                Image: <input type="file" name="image"><br><br>
+                <img src="' . $user['image'] . '" width="100" height="100" alt="User Image"><br><br>
+                <input type="submit" name="submit" value="Save Changes"></form>
+
+            <?php
         } else {
             echo 'Invalid user ID.';
         }
@@ -67,7 +70,8 @@ class UserEditor
         }
     }
 
-    public  function getUserData(){
+    public function getUserData()
+    {
         return $this->userData;
     }
 }
