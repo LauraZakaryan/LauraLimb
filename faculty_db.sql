9e1d3389cb84f05f0aeca80ed54cca0f9a4ac8b3
LauraZakaryan <146218796+LauraZakaryan@users.noreply.github.com>
2023-11-22 18:12:33 +0400

    Update faculty_db.sql

--- faculty_db.sql
+++ faculty_db.sql
@@ -35,41 +35,41 @@ CREATE TABLE `mog` (
 ) ENGINE = INNODB;
 
 CREATE TABLE `user_courses` (
-    `id` INT,
+    `user_id` INT,
     `course_id` INT,
-    PRIMARY KEY (`id`, `course_id`),
-    FOREIGN KEY (`id`) REFERENCES `users`(`id`),
+    PRIMARY KEY (`user_id`, `course_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
     FOREIGN KEY (`course_id`) REFERENCES `courses`(`id`)
 ) ENGINE = INNODB;
 
 CREATE TABLE `user_streams` (
-    `id` INT,
+    `user_id` INT,
     `stream_id` INT,
-    PRIMARY KEY (`id`, `stream_id`),
-    FOREIGN KEY (`id`) REFERENCES `users`(`id`),
+    PRIMARY KEY (`user_id`, `stream_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
     FOREIGN KEY (`stream_id`) REFERENCES `streams`(`id`)
 ) ENGINE = INNODB;
 
 CREATE TABLE `user_groups` (
-    `id` INT,
+    `user_id` INT,
     `group_id` INT,
-    PRIMARY KEY (`id`, `group_id`),
-    FOREIGN KEY (`id`) REFERENCES `users`(`id`),
+    PRIMARY KEY (`user_id`, `group_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
     FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`)
 ) ENGINE = INNODB;
 
 CREATE TABLE `user_salary` (
-    `id` INT,
+    `user_id` INT,
     `salary_id` INT,
-    PRIMARY KEY (`id`, `salary_id`),
-    FOREIGN KEY (`id`) REFERENCES `users`(`id`),
+    PRIMARY KEY (`user_id`, `salary_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
     FOREIGN KEY (`salary_id`) REFERENCES `salary`(`id`)
 ) ENGINE = INNODB;
 
 CREATE TABLE `user_mog` (
-    `id` INT,
+    `user_id` INT,
     `mog_id` INT,
-    PRIMARY KEY (`id`, `mog_id`),
-    FOREIGN KEY (`id`) REFERENCES `users`(`id`),
+    PRIMARY KEY (`user_id`, `mog_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
     FOREIGN KEY (`mog_id`) REFERENCES `mog`(`id`)
 ) ENGINE = INNODB;
