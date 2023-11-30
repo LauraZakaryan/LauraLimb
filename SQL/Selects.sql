c88bd37d7a07b6a7a5befb02f22ea8e41d725c1e
LauraZakaryan <146218796+LauraZakaryan@users.noreply.github.com>
2023-11-30 19:19:38 +0400

    Update Selects.sql

--- SQL/Selects.sql
+++ SQL/Selects.sql
@@ -1,13 +1,16 @@
 # Select all the users who's names ( f name or l name ) contains letter "a"
 SELECT `first_name`, `last_name`
 FROM `users`
-WHERE `first_name`  LIKE '%a%' OR `last_name` LIKE '%a%';;
+WHERE `first_name`  LIKE '%a%' OR `last_name` LIKE '%a%';
 
 # Select all the groups which studies "English"
-SELECT `groups`.`name`, `schedule`.`group_id`
-FROM `groups`
-LEFT JOIN `schedule` ON `groups`.id =`schedule`.group_id
-WHERE `subject_id`='22';
+SELECT 
+     `groups`.`name`, 
+	  `schedule`.`group_id`
+FROM `schedule`
+INNER JOIN `groups` ON `groups`.id =`schedule`.group_id
+INNER JOIN `subjects`ON `subjects`.`id` = `schedule`.`subject_id`
+WHERE `subjects`.`name`='English';
 
 # Select groups . Columns: group's id, group's name and student count. Each row should be like this, ex: ->12, "Xumb 1", "24"
 SELECT
@@ -19,7 +22,7 @@ FROM
 LEFT JOIN
     `user_groups`  ON `groups`.`id` = `user_groups`.`group_id`
 GROUP BY
-   `groups`.`id`, `groups`.`name`;
+   `groups`.`id`;
    
 /*Select courses. Those courses which have groups which have studies that are taught by Navasardyan and the days of classes are at Monday and Friday.
  Columns to show: Kurs id , kurs name, group name,  group id, the date of the class, and the time of the class*/
@@ -31,8 +34,12 @@ SELECT
     `schedule`.`day_of_week` AS `the date of the class`,
     `schedule`.`class_id` AS `the time of the class`
 FROM `schedule`
-JOIN 
+INNER JOIN 
      `courses` ON `courses`.`id` = `schedule`.`course_id` 
-JOIN 
+INNER JOIN 
      `groups` ON `groups`.`id` = `schedule`.`group_id` 
-WHERE `schedule`.`user_id` = '3' AND  `schedule`.`day_of_week` IN ('Monday', 'Friday');
+INNER JOIN 
+     `users` ON `users`.`id` = `schedule`.`user_id`
+WHERE `users`.`last_name` = 'Navasardyan' AND  `schedule`.`day_of_week` IN ('Monday', 'Friday')
+GROUP BY 
+     `groups`.`id`;
